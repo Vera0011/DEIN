c9971d6fd0f18aa77f3d7f563c732b91127ab934
Vera0011 <contact.vera0011@gmail.com>
2023-11-16 20:10:17 +0100

    Finished database connection

--- SQL_Server-Connection/MainWindow.xaml.cs
+++ SQL_Server-Connection/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace SQL_Server_Connection
 {
@@ -20,9 +22,31 @@ namespace SQL_Server_Connection
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
         public MainWindow()
         {
             InitializeComponent();
+
+            string connection = ConfigurationManager.ConnectionStrings["SQL_Server_Connection.Properties.Settings.alumta_deinConnectionString"].ConnectionString;
+
+            sqlConnection = new SqlConnection(connection);
+
+            this.queryClients();
+        }
+
+        private void queryClients()
+        {
+            string query = "SELECT * FROM Usuarios WHERE user_type='user'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            using(adapter)
+            {
+                System.Data.DataTable users = new System.Data.DataTable();
+
+                adapter.Fill(users);
+
+                listClients.ItemsSource = users.DefaultView;
+            }
         }
     }
 }
