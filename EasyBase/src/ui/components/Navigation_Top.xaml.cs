7d99618284f0c0c36b7f33e4eda6fb1a82fdf2ae
Vera0011 <contact.vera0011@gmail.com>
2023-11-17 19:19:17 +0100

    Initial project

---
+++ EasyBase/src/ui/components/Navigation_Top.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyBase.src.ui.components
+{
+    /// <summary>
+    /// Lógica de interacción para Navigation_Top.xaml
+    /// </summary>
+    public partial class Navigation_Top : UserControl
+    {
+        public Navigation_Top()
+        {
+            InitializeComponent();
+        }
+    }
+}
